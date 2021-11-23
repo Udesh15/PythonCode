4a0477147640c99096947de9a682f58ab7445a7e
udesh15 <udeshjuseja@gmail.com>
2021-11-23 19:06:03 +0530

    Calculator

--- Project1-Fault.py
+++ Project1-Fault.py
@@ -3,7 +3,6 @@ def addition(a, b):
     return (c)
     # print(c)
 
-
 def subtraction(a, b):
     c = b - a
     return (c)
@@ -23,14 +22,23 @@ def OperationAsperUser():
     if Opera == 'addition':
         c = addition(a, b)
         print(c)
+        Rerun()
     elif Opera == 'subtraction':
         subtraction(a, b)
     elif Opera == 'multiply':
         multiplication(a, b)
     elif Opera == 'division':
         division(a, b)
+def Rerun():
+    RunAgain = input("If You want to do Any otherOperation Press 'Y'")
+    if RunAgain == 'Y' :
+        OperationAsperUser()
+    else :
+        print("Thank You For Using the Calculator!")
 
 OperationAsperUser()
+
+
 
 
 
