d9b48d81532aafc13c29a33cf51f3836fb4bb457
udesh15 <udeshjuseja@gmail.com>
2021-11-27 01:40:17 +0530

    Basic Factorial

---
+++ FactorialRecursion.py
@@ -0,0 +1,19 @@
+def Factorial(N):
+    #N = int(input("Enter the Value for factorial : "))
+    Fact = 0
+
+    print(N)
+    N -= 1
+
+    if N > 0:
+        Factorial(N)
+
+
+Factorial(3)
+
+
+
+
+
+
+
